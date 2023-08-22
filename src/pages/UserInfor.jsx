f3c1299c463e72b345b9cb7a47eb062700ac8c34
Devil Asura <lamtho1999@gmail.com>
2023-08-22 22:07:05 +0700

    Update star

--- src/pages/UserInfor.jsx
+++ src/pages/UserInfor.jsx
@@ -5,7 +5,6 @@ import {
   EllipsisOutlined,
   SettingOutlined,
   StarFilled,
-  StarOutlined,
 } from '@ant-design/icons';
 
 export const UserInfor = (info) => {
@@ -32,19 +31,19 @@ export const UserInfor = (info) => {
         <Skeleton loading={loading} avatar active>
           <Meta
             avatar={
-              <Avatar src='https://scontent-xsp1-1.xx.fbcdn.net/v/t1.18169-9/15726431_128651384298117_6174205444485405726_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=bjsaxiWx4zAAX-Jx641&_nc_ht=scontent-xsp1-1.xx&oh=00_AfDtN5Ec3mHnaYJ1mTQfbmMBYZyJ2fHLKpZPHChyN9-B5w&oe=650C292A' />
+              <Avatar src='https://i.pinimg.com/564x/b6/74/44/b67444cabdb5aaadb6735e75df1bcc5c.jpg' />
             }
             title={info.userName ? info.userName : 'Shipper'}
-            description='Đm wibu'
+            description='Nhân viên giao hàng'
           />
           <div style={{ marginTop: 16 }}>
             <StarFilled style={{ color: '#FFF700' }} />
-            <StarOutlined style={{ color: '#FFF700' }} />
-            <StarOutlined style={{ color: '#FFF700' }} />
-            <StarOutlined style={{ color: '#FFF700' }} />
-            <StarOutlined style={{ color: '#FFF700' }} />
+            <StarFilled style={{ color: '#FFF700' }} />
+            <StarFilled style={{ color: '#FFF700' }} />
+            <StarFilled style={{ color: '#FFF700' }} />
+            <StarFilled style={{ color: '#FFF700' }} />
           </div>
-          <div style={{ marginTop: 16 }}>Additional Infor</div>
+          <div style={{ marginTop: 16 }}>Thông tin chi tiết</div>
         </Skeleton>
       </Card>
     </div>
